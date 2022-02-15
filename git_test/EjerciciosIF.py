02a7d8cb028c67539bae49b24a75c13a0a85757e
Wendy <88361194+wendy192@users.noreply.github.com>
2022-02-14 18:33:46 -0600

    Ejercicios de 14 de febrero 2022

---
+++ git_test/EjerciciosIF.py
@@ -0,0 +1,41 @@
+#Ejercicios de if
+#Determinar el mayor entre dos numeros, tomando en cuenta si son iguales
+"""
+print("Determina el mayor de dos numeros")
+
+x= int(input("ingresa el primer numero  "))
+y= int(input("ingresa el segundo numero  "))
+
+if x<y:
+    print("el numero mayor es ",y)
+elif y<x:
+    print("el numero mayor es ",x)
+else:
+    print("los numeros son iguales")
+"""
+###################
+#Requerir al usuario que ingrese un día de la semana e 
+#imprimir un mensaje si es lunes, otro mensaje diferente si es viernes, 
+#otro mensaje diferente si es sábado o domingo. 
+#Si el día ingresado no es ninguno de esos, imprimir otro mensaje
+
+print("Dia de la semana")
+print("Escoge uno de los siguientes dias de la semana, ingresa su numero ")
+#le doy a escoger al usuario con numeros para evitar los posibles problemas con las mayusculas
+#y los acentos
+str=1,2
+1=="Lunes"
+2=="Martes"
+print("1.-Lunes")
+print("2.-Martes")
+print("3.-Miercoles")
+print("4.-Jueves")
+print("5.-Viernes")
+print("6.-Sabado")
+print("7.-Domingo")
+#dia=str(input(">>> ")
+#if dia ==1:
+ #   print("Escogiste el mejor dia, unicia la semana con "+ 1)
+#else: 
+  #  print("Un buen dia para tomar el sol es el "+2)
+
---
+++ git_test/NUmero par e impar.py
@@ -0,0 +1,18 @@
+
+################################
+# Determina si el numero ingresado es par o impar
+
+numero=int(input("Ingresa el numero  "))
+if (numero%2)==0:
+    print("El numero " + str(numero) + " es par")
+else:
+    print("El numero " +str(numero)+ " es impar")
+
+###########
+
+
+
+
+
+
+        
