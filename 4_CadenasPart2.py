3718bc5b00fcb68069c0041c49b9060bd49f6569
Wendy <88361194+wendy192@users.noreply.github.com>
2022-03-17 20:37:10 -0600

    Ejercicios del 17 de marzo

---
+++ 4_CadenasPart2.py
@@ -0,0 +1,28 @@
+#1.Concatene la cadena 'Treinta', 'Ejercicios', 'De', 'Python' en una sola cadena,
+#  'Treinta ejercicios de Python'.
+str1="Treinta "
+str2="ejercicios "
+str3="de "
+str4="Python "
+
+print("{}{}{}{}".format(str1,str2,str3,str4))
+
+
+#2.Concatene la cadena 'Codificación', 'Para', 'Todos' en una sola cadena,
+#  'Codificación para todos'.
+str5="Codificación "
+str6="para "
+str7="todos "
+print("{}{}{}".format(str5,str6,str7))
+
+#3.Declare una variable llamada empresa y asígnele un valor inicial
+#  "Codificación para todos".
+
+empresa="Codificacion para todos"
+
+#4.Imprime la empresa variable usando print() .
+print(empresa)
+
+#5.Imprima la longitud de la cadena de caracteres de la empresa 
+# utilizando el método len() e print() .
+print(len(empresa))
