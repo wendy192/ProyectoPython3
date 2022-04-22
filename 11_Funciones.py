535015d29262070629d8fe5536dada01129cde8d
Wendy <88361194+wendy192@users.noreply.github.com>
2022-04-21 21:26:29 -0500

    .

---
+++ 11_Funciones.py
@@ -0,0 +1,35 @@
+"""
+
+def  generar_nombre_completo ():
+        nombre = 'Asabeneh' 
+        apellido  = ' Yetayeh ' 
+        espacio = ' ' 
+        nombre_completo = nombre + espacio + apellido 
+        print ( nombre_completo )
+generar_nombre_completo () 
+
+def  sumar_dos_numeros ():
+     num_uno  =  2
+     num_dos  =  3 
+     total  =  num_uno  +  num_dos 
+     print ( total )
+
+sumar_dos_numeros ()
+
+def saludos(nombre):
+        mensaje=nombre+'Bienvenidos'
+        return mensaje
+print (saludos ('FUlanita '))
+
+def sumar_diez(num):
+        diez=10
+        return num+ diez
+print(sumar_diez(90))
+"""
+
+def greetings (name = 'Peter'):
+    message = name + ', welcome to Python for Everyone!'
+    return message
+print(greetings())
+print(greetings('Asabeneh'))
+
