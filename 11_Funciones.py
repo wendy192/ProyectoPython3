1665c54f5ba6ceba607f05478ead702b8d7d1112
Wendy <88361194+wendy192@users.noreply.github.com>
2022-04-23 11:33:29 -0500

    .

--- 11_Funciones.py
+++ 11_Funciones.py
@@ -25,11 +25,15 @@ def sumar_diez(num):
         diez=10
         return num+ diez
 print(sumar_diez(90))
-"""
+
 
 def greetings (name = 'Peter'):
     message = name + ', welcome to Python for Everyone!'
     return message
 print(greetings())
 print(greetings('Asabeneh'))
-
+"""
+def pesoobjeto(masa,gravedd=9.81):
+        peso=str(masa*gravedd)+'N'
+        return peso
+print('El peso de un objeto en Newtons es ', pesoobjeto(100,1.62     ))
